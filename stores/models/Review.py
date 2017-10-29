aed7a974dd8919045aaada5121a8f73afed72781
Lord Hung <hunguit14@gmail.com>
2017-10-30 02:22:56 +0700

    Add Store and StoreReview model

---
+++ stores/models/Review.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+import Store
+
+User = settings.AUTH_USER_MODEL
+
+
+#
+# Combine Rating and Comment in One Table
+# reason: https://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/
+#
+class StoreReview(models.Model):
+    user = models.ForeignKey(User)
+    store = models.ForeignKey(Store)
+    rating = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
+    comment = models.TextField(default='', blank=True, null=True)
+    approved = models.BooleanField(default=1)
+    spam = models.BooleanField(default=0)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
---
+++ stores/models/Store.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+# Dùng AUTH_USER_MODEL để set quan hệ, ko dùng get_user_model()
+# reason: https://stackoverflow.com/questions/24629705/django-using-get-user-model-vs-settings-auth-user-model
+User = settings.AUTH_USER_MODEL
+
+
+class Store(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.OneToOneField(User)
+    views_count = models.PositiveIntegerField(default=0, blank=True, null=True)
+    reviews_count = models.PositiveIntegerField(default=0, blank=True, null=True)
+    rating_cache = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
