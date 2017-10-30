35f245abb978bf608d14c99d144fd1d28a90da2f
Lord Hung <hunguit14@gmail.com>
2017-10-31 01:25:31 +0700

    Add StoreManager for Store model

--- stores/models/Store.py
+++ stores/models/Store.py
@@ -1,6 +1,7 @@
 import os
 import random
 from django.db import models
+from django.db.models import Q
 from django.conf import settings
 
 # Dùng AUTH_USER_MODEL để set quan hệ, ko dùng get_user_model()
@@ -21,6 +22,33 @@ def upload_image_path(instance, filename):
     return f'stores/{new_filename}/{final_filename}'
 
 
+class StoreQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def search(self, query):
+        lookups = Q(title__icontains=query)  # Q(tag__title__icontains=query))
+        # shop abc, xyz, quan ao, giay dep
+        return self.filter(lookups).distinct()
+
+
+class StoreManager(models.Manager):
+    def get_queryset(self):
+        return StoreQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)  # Store.objects == self.get_queryset()
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+    def search(self, query):
+        return self.get_queryset().active().search(query)
+
+
 class Store(models.Model):
     title = models.CharField(max_length=255)
     user = models.OneToOneField(User)
@@ -31,6 +59,8 @@ class Store(models.Model):
     rating_cache = models.PositiveSmallIntegerField(default=0, null=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = StoreManager()
     
     class Meta:
         db_table = 'store'
