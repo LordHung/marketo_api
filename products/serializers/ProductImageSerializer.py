967ae5c16b2c68407dbbc31b2d1c6e299b923051
Lord Hung <hunguit14@gmail.com>
2017-12-06 14:21:08 +0700

    Fix error no product related

--- products/serializers/ProductImageSerializer.py
+++ products/serializers/ProductImageSerializer.py
@@ -5,4 +5,4 @@ from ..models import ProductImage
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
-        fields = ('image',)
+        fields = '__all__'
