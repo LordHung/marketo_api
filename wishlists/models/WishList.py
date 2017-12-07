43406c307d6276d4b75b4448c58c1d6321fc85dd
Lord Hung <hunguit14@gmail.com>
2017-12-08 03:30:25 +0700

    Add Wislist module

---
+++ wishlists/models/WishList.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+# from products.models import Product
+
+User = settings.AUTH_USER_MODEL
+
+
+class WishList(models.Model):
+    user = models.OneToOneField(User)
+    products = models.ManyToManyField('products.Product', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
---
+++ wishlists/models/__init__.py
@@ -0,0 +1,1 @@
+from .WishList import WishList
