daba86454db2c0ee89aaccb4d988d6b986257dcd
Lord Hung <hunguit14@gmail.com>
2017-12-08 11:00:47 +0700

    Change wishlist o2o user to fk user

--- wishlists/models/WishList.py
+++ wishlists/models/WishList.py
@@ -6,7 +6,7 @@ User = settings.AUTH_USER_MODEL
 
 
 class WishList(models.Model):
-    user = models.OneToOneField(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField('products.Product', blank=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
