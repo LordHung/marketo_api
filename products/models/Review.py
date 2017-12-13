8a98f3cc8a80a18992e8f50ee6794a86c6e003b5
Lord Hung <hunguit14@gmail.com>
2017-12-13 22:45:09 +0700

    Make rating can be chosen

--- products/models/Review.py
+++ products/models/Review.py
@@ -3,11 +3,13 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
+RATING = ((0.5, 0.5), (1, 1), (1.5, 1.5), (2, 2), (2.5, 2.5), (3, 3), (3.5, 3.5), (4, 4), (4.5, 4.5), (5, 5))
+
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
+    rating = models.FloatField(default=0, choices=RATING, blank=True, null=True)
     comment = models.TextField(default='', blank=True, null=True)
     approved = models.BooleanField(default=True)
     spam = models.BooleanField(default=False)
