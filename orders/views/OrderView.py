2e17a2dd79d78f80d8a2e3017ed6170c062eaddd
Lord Hung <hunguit14@gmail.com>
2018-01-07 01:24:49 +0700

    Add get orders by customerid

--- orders/views/OrderView.py
+++ orders/views/OrderView.py
@@ -8,7 +8,25 @@ from ..serializers import OrderSerializer
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    # parser_classes = (FormParser, )
+
+    def list(self, request, *args, **kwargs):
+        customer_id = None
+        queryset = Order.objects.all()
+        params = request.query_params
+        try:
+            if 'customer-id' in params:
+                customer_id = params['customer-id']
+                queryset = Order.objects.filter(customer=customer_id)
+        except MultiValueDictKeyError:
+            queryset = Order.objects.all()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
     	print(self.request)
