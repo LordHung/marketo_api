e08b569bb77aeb2d0efe6ed89ccd9d64968a7c54
Lord Hung <hunguit14@gmail.com>
2018-01-09 02:34:41 +0700

    Refactor product_count for category

--- products/serializers/CategorySerializer.py
+++ products/serializers/CategorySerializer.py
@@ -9,21 +9,9 @@ from .ProductSerializer import ProductSerializer
 class CategorySerializer(ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='category-detail')
     product_set = ProductSerializer(many=True, read_only=True)
-    product_count = serializers.SerializerMethodField()
+    product_count = serializers.IntegerField(source='product_set.count')
     
     class Meta:
         model = Category
         fields = ('url', 'id', 'name', 'active', 'store', 'parent', 'product_count',
                   'image', 'description', 'timestamp', 'product_set', )
-
-    def get_product_count(self, obj):
-        product_count = 0
-
-        if obj.parent:  # its a subcategory
-            product_count = obj.product_set.count()
-            return product_count
-        else:  # its a main category
-            # count products in subcategory
-            for category in Category.objects.filter(id__in=[obj.id, obj.parent]).distinct():
-                product_count += category.product_set.count()
-            return product_count
