72715f01e0943af215d50f7ececa6e43c364f313
Lord Hung <hunguit14@gmail.com>
2018-01-16 00:18:17 +0700

    Add featured_images for store

--- stores/serializers/StoreSerializer.py
+++ stores/serializers/StoreSerializer.py
@@ -4,8 +4,8 @@ from rest_framework import serializers
 from accounts.serializers import UserSerializer
 
 from ..models import Store
-from products.models import Product, Review
-from products.serializers import ProductSerializer
+from products.models import Product, Review, Image
+from products.serializers import ProductSerializer, ImageSerializer
 
 User = get_user_model()
 
@@ -18,20 +18,39 @@ class StoreSerializer(serializers.ModelSerializer):
     discounts = serializers.SerializerMethodField()
     topratings = serializers.SerializerMethodField()
     topsellers = serializers.SerializerMethodField()
+    featured_images = serializers.SerializerMethodField()
 
     class Meta:
         model = Store
         fields = ('id', 'url', 'name', 'user', 'icon', 'active', 'product_count',
-                  'discount_count', 'discounts', 'topratings', 'topsellers', 'timestamp', 'updated', )
+                  'discount_count', 'discounts', 'topratings', 'topsellers', 'timestamp', 'updated', 'featured_images')
 
     def get_discounts(self, obj):
-        discounts = obj.product_set.filter(on_sale=True)
+        discounts = obj.product_set.filter(on_sale=True).order_by('-sale_rate')
         return ProductSerializer(discounts, many=True, context={'request': self.context['request']}).data
 
     def get_topratings(self, obj):
-        tr = obj.product_set.order_by('-average_rating')
+        tr = obj.product_set.filter(average_rating__gte=4).order_by('-average_rating')
         return ProductSerializer(tr, many=True, context={'request': self.context['request']}).data
 
     def get_topsellers(self, obj):
-        ts = obj.product_set.order_by('-sold')
+        ts = obj.product_set.filter(sold__gte=1).order_by('-sold')
         return ProductSerializer(ts, many=True, context={'request': self.context['request']}).data
+
+    def get_featured_images(self, obj):
+        try:
+            featured_dc = obj.product_set.filter(on_sale=True).order_by('-sale_rate').first().image_set.all()[:1]
+        except:
+            featured_dc = Image.objects.none()
+        try:
+            featured_tr = obj.product_set.filter(average_rating__gte=4).order_by('-average_rating').first().image_set.all()[:1]
+        except:
+            featured_tr = Image.objects.none()
+        try:
+            featured_ts = obj.product_set.filter(sold__gte=1).order_by('-sold').first().image_set.all()[:1]
+        except:
+            featured_ts = Image.objects.none()
+
+        featured_images = featured_dc.union(featured_tr, featured_ts, all=True)
+
+        return ImageSerializer(featured_images, many=True, context={'request': self.context['request']}).data
