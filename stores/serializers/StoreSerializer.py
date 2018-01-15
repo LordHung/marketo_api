5550eae4e8e6d9a68ba217b7a24666c514d44a0c
Lord Hung <hunguit14@gmail.com>
2018-01-15 16:16:51 +0700

    Add get top ratings and top sellers

--- stores/serializers/StoreSerializer.py
+++ stores/serializers/StoreSerializer.py
@@ -13,24 +13,25 @@ User = get_user_model()
 class StoreSerializer(serializers.ModelSerializer):
     active = serializers.BooleanField(initial=True)
     url = serializers.HyperlinkedIdentityField(view_name='store-detail')
-    product_count = serializers.IntegerField(source='product_set.count')
-    discount_count = serializers.IntegerField(source='get_discounts_count')
+    product_count = serializers.IntegerField(source='product_set.count', read_only=True)
+    discount_count = serializers.IntegerField(source='get_discounts_count', read_only=True)
     discounts = serializers.SerializerMethodField()
-    highratings = serializers.SerializerMethodField()
-    bestsellers = serializers.SerializerMethodField()
+    topratings = serializers.SerializerMethodField()
+    topsellers = serializers.SerializerMethodField()
 
     class Meta:
         model = Store
         fields = ('id', 'url', 'name', 'user', 'icon', 'active', 'product_count',
-                  'discount_count', 'discounts', 'highratings', 'bestsellers', 'timestamp', 'updated', )
+                  'discount_count', 'discounts', 'topratings', 'topsellers', 'timestamp', 'updated', )
 
     def get_discounts(self, obj):
         discounts = obj.product_set.filter(on_sale=True)
         return ProductSerializer(discounts, many=True, context={'request': self.context['request']}).data
 
-    def get_highratings(self, obj):
-        return None
+    def get_topratings(self, obj):
+        tr = obj.product_set.order_by('-average_rating')
+        return ProductSerializer(tr, many=True, context={'request': self.context['request']}).data
 
-    def get_bestsellers(self, obj):
-        return None
-
+    def get_topsellers(self, obj):
+        ts = obj.product_set.order_by('-sold')
+        return ProductSerializer(ts, many=True, context={'request': self.context['request']}).data
