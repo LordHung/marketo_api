10fee66d20f8d6a42cf67c7ac0eb2cee9ad76ceb
Lord Hung <hunguit14@gmail.com>
2018-01-16 18:58:18 +0700

    Add Store admin

--- stores/admin.py
+++ stores/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Store
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    fields = ('name', 'user', 'active', 'icon', 'views_count', 'reviews_count', 'rating_cache', )
+    list_display = ('id', 'user', 'name', 'active', 'views_count', 'reviews_count', )
+    readonly_fields = ('views_count', 'reviews_count', 'rating_cache', )
