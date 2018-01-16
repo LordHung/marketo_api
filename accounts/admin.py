cbb473f30794b924d6bead84aa155e40007ab35e
Lord Hung <hunguit14@gmail.com>
2018-01-16 16:57:47 +0700

    Add user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('email', 'password', 'full_name', 'is_active', 'staff', 'admin', )
+    list_display = ('id', 'email', 'full_name', 'is_active', 'staff', 'admin', )
+    list_display_links = ('id', 'email', 'full_name', )
+    ordering = ('id', )
